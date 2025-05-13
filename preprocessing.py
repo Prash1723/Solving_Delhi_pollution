5e1fc206d8799d42da01c0cfee5495dc692e9efc
praveen <s.praveen795@gmail.com>
2025-05-13 21:58:13 +0530

    preprocessing\ pollutant list

--- preprocessing.py
+++ preprocessing.py
@@ -12,4 +12,13 @@ df['date'] = pd.to_datetime(df['date'], format="%Y-%m-%d")
 # Year
 df['year'] = df['date'].apply(lambda x: int(str(x).split('-')[0]))
 
+# Create list of unique pollutants
+values = []
+combo_pollutants = df['Prominent Pollutant'].apply(lambda x: x.split(', ')).tolist()		# Combination of pollutants
+
+for i in combo_pollutants:
+	values.extend(i)
+
+pollutants = list(set(values))																# Unique pollutants
+
 df.to_csv('data/preprocessed.csv', index=False)
