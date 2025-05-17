201a01a01a5bba53ed933e12b9e99dd57f93f4bc
praveen <s.praveen795@gmail.com>
2025-05-17 23:50:04 +0530

    Update\ seasonality

--- preprocessing.py
+++ preprocessing.py
@@ -13,6 +13,26 @@ df['year'] = df['date'].apply(lambda x: int(str(x).split('-')[0]))
 # Air Quality
 df['Air Quality'] = df['Air Quality'].apply(lambda s: 'Very Poor' if 'Very poor' in s else s)
 
+# Season
+season_dict = {
+	'1': "winter",
+	'2': "winter",
+	'3': "summer",
+	'4': "summer",
+	'5': "summer",
+	'6': "summer",
+	'7': "monsoon",
+	'8': "monsoon",
+	'9': "monsoon",
+	'10': "monsoon",
+	'11': "winter",
+	'12': "winter"
+}
+
+df['month'] = df['date'].dt.month
+
+df['season'] = df['month'].apply(lambda x : season_dict.get(str(x), x))
+
 # Create list of unique pollutants
 values = []
 combo_pollutants = df['Prominent Pollutant'].apply(lambda x: x.split(', ')).tolist()		# Combination of pollutants
