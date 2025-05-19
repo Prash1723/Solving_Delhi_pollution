804d6f48f87a065abf2202fa624beadd4fa7337a
praveen <s.praveen795@gmail.com>
2025-05-19 23:28:58 +0530

    Update\ date_range for All

--- script.py
+++ script.py
@@ -136,12 +136,12 @@ def update_chart1():
 	if selected_pollutant != "All" and selected_season != "All":
 		# Trend line
 		filtered_data1 = df.query(
-			"`Prominent Pollutant` == @selected_pollutant and season == @selected_season"
+			"`Prominent Pollutant` == @selected_pollutant and season == @selected_season and year>=@slider_lr and year<=slider_hr"
 			)
 
 		# Pollutant presence
 		filtered_data2 = df.query(
-			"`Prominent Pollutant` == @selected_pollutant and season == @selected_season"
+			"`Prominent Pollutant` == @selected_pollutant and season == @selected_season and year>=@slider_lr and year<=slider_hr"
 			)[[
 		"SO2",
 		"O3",
@@ -156,7 +156,7 @@ def update_chart1():
 
 		# Air Quality
 		filtered_data3 = df.query(
-			"`Prominent Pollutant` == @selected_pollutant and season == @season_select"
+			"`Prominent Pollutant` == @selected_pollutant and season == @season_select and year>=@slider_lr and year<=slider_hr"
 			).groupby('Air Quality')[
 		'date'
 		].count().reset_index()
@@ -165,10 +165,10 @@ def update_chart1():
 
 	elif selected_pollutant == "All" and selected_season != "All":
 		# Trend line
-		filtered_data1 = df.query("season == @selected_season")
+		filtered_data1 = df.query("season == @selected_season and year>=@slider_lr and year<=slider_hr")
 
 		# Pollutant presence
-		filtered_data2 = df.query("season == @selected_season")[[
+		filtered_data2 = df.query("season == @selected_season and year>=@slider_lr and year<=slider_hr")[[
 		"SO2",
 		"O3",
 		"PM10",
@@ -181,7 +181,7 @@ def update_chart1():
 		filtered_data2.columns = ['pollutants', 'days']
 
 		# Air Quality
-		filtered_data3 = df.query("season == @selected_season").groupby('Air Quality')[
+		filtered_data3 = df.query("season == @selected_season and year>=@slider_lr and year<=slider_hr").groupby('Air Quality')[
 		'date'
 		].count().reset_index()
 
@@ -189,10 +189,10 @@ def update_chart1():
 
 	elif selected_pollutant != "All" and selected_season == "All":
 		# Trend line
-		filtered_data1 = df.query("`Prominent Pollutant` == @selected_pollutant")
+		filtered_data1 = df.query("`Prominent Pollutant` == @selected_pollutant and year>=@slider_lr and year<=slider_hr")
 
 		# Pollutant presence
-		filtered_data2 = df.query("`Prominent Pollutant` == @selected_pollutant")[[
+		filtered_data2 = df.query("`Prominent Pollutant` == @selected_pollutant and year>=@slider_lr and year<=slider_hr")[[
 		"SO2",
 		"O3",
 		"PM10",
@@ -205,7 +205,9 @@ def update_chart1():
 		filtered_data2.columns = ['pollutants', 'days']
 
 		# Air Quality
-		filtered_data3 = df.query("`Prominent Pollutant` == @sellected_pollutant").groupby('Air Quality')[
+		filtered_data3 = df.query(
+			"`Prominent Pollutant` == @sellected_pollutant and year>=@slider_lr and year<=slider_hr"
+			).groupby('Air Quality')[
 		'date'
 		].count().reset_index()
 
