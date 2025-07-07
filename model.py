6041b563d45560fbe719e1440329cfc6032d2199
praveen <s.praveen795@gmail.com>
2025-07-07 23:49:55 +0530

    Update\ train test split

--- model.py
+++ model.py
@@ -5,8 +5,10 @@ from sklearn.model_selection import train_test_split
 data = pd.read_csv(r'data/preprocessed.csv')
 
 # Select features for model
-x = data[['Prominent Pollutant', 'year', 'month', 'SO2', 'NO2', 'O3', 'PM10', 'CO', 'OZONE', 'PM2.5']]
+X = data[['Prominent Pollutant', 'year', 'month', 'SO2', 'NO2', 'O3', 'PM10', 'CO', 'OZONE', 'PM2.5']]
 
 y = data['Index Value']
 
+# Train test split
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
 
