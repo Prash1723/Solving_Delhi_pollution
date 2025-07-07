3b1594691927871b1acd5ddc2bc0b3294381caf1
praveen <s.praveen795@gmail.com>
2025-07-07 23:44:11 +0530

    Update\ model features

--- model.py
+++ model.py
@@ -1,4 +1,8 @@
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 
-df = pd.read_csv(r'data/preprocessed.csv')
+data = pd.read_csv(r'data/preprocessed.csv')
+
+# Select features for model
+df = data[['Index Value', 'Prominent Pollutant', 'year', 'month', 'SO2', 'NO2', 'O3', 'PM10', 'CO', 'OZONE', 'PM2.5']]
+
