b492c7324e7e96436eeca3a2b215ac76c16aacf7
praveen <s.praveen795@gmail.com>
2025-07-07 23:48:46 +0530

    Update\ split features

--- model.py
+++ model.py
@@ -1,8 +1,12 @@
 import pandas as pd
 from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
 
 data = pd.read_csv(r'data/preprocessed.csv')
 
 # Select features for model
-df = data[['Index Value', 'Prominent Pollutant', 'year', 'month', 'SO2', 'NO2', 'O3', 'PM10', 'CO', 'OZONE', 'PM2.5']]
+x = data[['Prominent Pollutant', 'year', 'month', 'SO2', 'NO2', 'O3', 'PM10', 'CO', 'OZONE', 'PM2.5']]
+
+y = data['Index Value']
+
 
