0b74fa9a0dd4102cffc55a89d19456c6f111eafb
praveen <s.praveen795@gmail.com>
2025-07-08 22:35:57 +0530

    Update\ rmse

--- model.py
+++ model.py
@@ -1,6 +1,8 @@
 import pandas as pd
+import numpy as np
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error, mean_absolute_error
 
 data = pd.read_csv(r'data/preprocessed.csv')
 
@@ -20,3 +22,6 @@ lr.fit(X_train, y_train)
 y_pred = lr.predict(X_test)
 
 # Evaluate the model
+rmse = np.sqrt(mean_squared_error(y_test, y_pred))
+
+print("RMSE : {%f}", rmse)
