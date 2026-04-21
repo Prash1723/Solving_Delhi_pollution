866fca8ca144453a4896f3e3fcc033b626fe6494
praveen <s.praveen795@gmail.com>
2026-04-21 23:21:59 +0530

    Debug mode

--- script.py
+++ script.py
@@ -6,58 +6,87 @@ from bokeh.models import ColumnDataSource, Select, RangeSlider
 from bokeh.models import Tabs, TabPanel
 from bokeh.plotting import figure
 
-df = pd.read_csv(r'~/Projects/Solving_Delhi_pollution/data/preprocessed.csv')
+import logging
+from rich.console import Console
+from rich.logging import RichHandler
 
-print(df.info())
+rc = Console(record=True)
 
-# Source
-source1 = ColumnDataSource(df)
+# Set up the logger
+logging.basicConfig(
+    level="INFO",
+    format="%(message)s",
+    datefmt="[%X]",
+    handlers=[RichHandler()]
+)
 
-# Create widgets
-date_range = RangeSlider(
-	title="Select range of year",
-	value=(2015, 2023),
-	start=2015,
-	end=2023,
-	step=1
-	)
+log = logging.getLogger(__name__)
+log.setLevel(logging.DEBUG)
 
-pollutant_select = Select(
-	title="Select Pollutant",
-    value="All",
-	options=["All"] + list(set(df['Prominent Pollutant']))
-	)
+# 1. Console Handler (Rich)
+shell_handler = RichHandler()
+log.addHandler(shell_handler)
 
-# Figure
-p = figure(
-	x_axis_type="datetime",
-	title="AQI trend line",
-	height=400,
-	width=700
-	)
+file_handler = log.FileHandler("app.log")
+file_handler.setFormatter(log.Formatter("%(asctime)s - %(levelname)s - %(message)s"))
+log.addHandler(file_handler)
 
-p.line("date", "Index Value", source=source1, line_width=2)
-p.scatter("date", "Index Value", source=source1, size=3, color="red")
-p.xaxis.axis_label = "Date"
-p.yaxis.axis_label = "AQI Value"
+try:
+	df = pd.read_csv(r'~/Projects/Solving_Delhi_pollution/data/preprocessed.csv')
 
-# App
-def update_chart():
-	slider_value = date_range.value
-	slider_lr = slider_value[0]
-	slider_hr = slider_value[1]
-	selected_pollutant = pollutant_select.value
+	print(df.info())
 
-	if selected_pollutant != "All":
-		filtered_data = df[df["Prominent Pollutant"]==selected_pollutant]
+	# Source
+	source1 = ColumnDataSource(df)
 
-	source1.data.update() = ColumnDataSource.from_df(filtered_data)
+	# Create widgets
+	date_range = RangeSlider(
+		title="Select range of year",
+		value=(2015, 2023),
+		start=2015,
+		end=2023,
+		step=1
+		)
 
-#date_range.on_change("value", lambda attr, old, new: update_chart())
-#pollutant_select.on_change("value", lambda attr, old, new: update_chart())
+	pollutant_select = Select(
+		title="Select Pollutant",
+	    value="All",
+		options=["All"] + list(set(df['Prominent Pollutant']))
+		)
 
-# Layout
-#layout = column(row(date_range, pollutant_select), p)
+	# Figure
+	p = figure(
+		x_axis_type="datetime",
+		title="AQI trend line",
+		height=400,
+		width=700
+		)
 
-curdoc().add_root(p)
-curdoc().title = "AQI Dashboard"
+	p.line("date", "Index Value", source=source1, line_width=2)
+	p.scatter("date", "Index Value", source=source1, size=3, color="red")
+	p.xaxis.axis_label = "Date"	
+	p.yaxis.axis_label = "AQI Value"
+
+	# App
+	def update_chart():
+		slider_value = date_range.value
+		slider_lr = slider_value[0]
+		slider_hr = slider_value[1]
+		selected_pollutant = pollutant_select.value
+
+		if selected_pollutant != "All":
+			filtered_data = df[df["Prominent Pollutant"]==selected_pollutant]
+
+		source1.data.update() = ColumnDataSource.from_df(filtered_data)
+
+	#date_range.on_change("value", lambda attr, old, new: update_chart())
+	#pollutant_select.on_change("value", lambda attr, old, new: update_chart())
+
+	# Layout
+	#layout = column(row(date_range, pollutant_select), p)
+
+	curdoc().add_root(p)
+	curdoc().title = "AQI Dashboard"
+
+except as e:
+	log.info(f"Error: {e}")
